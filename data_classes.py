e23d723bc69c0704d242d55b6456048da0a0a23e
Daniel Guillen <dguillen@Daniels-MacBook-Pro.local>
2017-08-25 20:43:24 -0700

    initial datastore retrieval process has been added in main.py in order to test out and implement ajax functionality - eventually this compoment will be altered to randomly select lyric element as opposed to individually selecting elements

--- data_classes.py
+++ data_classes.py
@@ -1,14 +1,15 @@
 from google.appengine.ext import ndb
 
-class OneLineLyric(ndb.Model):
-    lyric = ndb.StringProperty()
-    upvotes = ndb.IntegerProperty()
-    song_key = ndb.KeyProperty(Song)
-    artist_key = ndb.KeyProperty(Artist)
-
-class Song(ndb.Model):
-    title = ndb.StringProperty()
-    artist_key = ndb.KeyPropery(Artist)
 
 class Artist(ndb.Model):
     name = ndb.StringProperty()
+
+class Song(ndb.Model):
+    title = ndb.StringProperty()
+    artist_key = ndb.KeyProperty(Artist)
+
+class OneLineLyric(ndb.Model):
+    lyric_text = ndb.StringProperty()
+    upvotes = ndb.IntegerProperty()
+    song_key = ndb.KeyProperty(Song)
+    artist_key = ndb.KeyProperty(Artist)
--- main.py
+++ main.py
@@ -18,6 +18,8 @@ import jinja2
 import os
 import webapp2
 import logging
+from google.appengine.ext import ndb
+from data_classes import OneLineLyric, Song, Artist
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -27,15 +29,45 @@ jinja_env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # NOTICE: These were temporarily stored in Datastore in order to test ajax
+        #   Ultimately, this section would randomly retrieve Datastore elements that were already stored in the Database
+        #   due to the Cron Tab and Execution (as opposed to manually storing them here)
+        #------------------------------------------------------------------------------------------------------#
+        # artist_temp = Artist(name = "Tyler, The Creator")
+        # artist_temp_key = artist_temp.put()
+        # song_temp = Song(title = "Where This Flower Blooms", artist_key = artist_temp_key)
+        # song_temp_key = song_temp.put()
+        # one_line_lyric_temp = OneLineLyric(lyric_text="I rock, I roll, I bloom, I glow",upvotes=0,song_key=song_temp_key,artist_key=artist_temp_key)
+        # lyric_key = one_line_lyric_temp.put()
+        #
+        # artist_temp = Artist(name = "Kendrick Lamar")
+        # artist_temp_key = artist_temp.put()
+        # song_temp = Song(title = "FEEL.", artist_key = artist_temp_key)
+        # song_temp_key = song_temp.put()
+        # one_line_lyric_temp = OneLineLyric(lyric_text="Ain't nobody prayin' for me",upvotes=0,song_key=song_temp_key,artist_key=artist_temp_key)
+        # lyric_key = one_line_lyric_temp.put()
+
         template = jinja_env.get_template('templates/main.html')
 
+        #NOTICE: Here specific lyric elements are individually selected as opposed to the eventual random selection charateristic
+        lyric_list = OneLineLyric.query().fetch()
+
+        left_lyric_el = lyric_list[0]
+        left_song_el = Song.query(Song.key == left_lyric_el.song_key).get()
+        left_artist_el = Artist.query(Artist.key == left_lyric_el.artist_key).get()
+
+        right_lyric_el = lyric_list[1]
+        right_song_el = Song.query(Song.key == right_lyric_el.song_key).get()
+        right_artist_el = Artist.query(Artist.key == right_lyric_el.artist_key).get()
+
         main_template_variables = {
-            'lyric_left': 'I rock, I roll, I bloom, I glow',
-            'song_name_left': 'Where This Flower Blooms',
-            'artist_name_left': 'Tyler, The Creator',
-            'lyric_right':"Ain't nobody prayin' for me",
-            'song_name_right':'FEEL.',
-            'artist_name_right':'Kendrick Lamar'
+            'lyric_left': left_lyric_el.lyric_text,
+            'song_name_left': left_song_el.title,
+            'artist_name_left': left_artist_el.name,
+
+            'lyric_right': right_lyric_el.lyric_text,
+            'song_name_right':right_song_el.title,
+            'artist_name_right':right_artist_el.name
         }
 
         self.response.write(template.render(main_template_variables))
