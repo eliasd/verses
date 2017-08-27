8f6b25c600b15c1ce511dcd25848ed03097b7807
Daniel Guillen <dguillen@Daniels-MacBook-Pro.local>
2017-08-26 19:30:18 -0700

    button functionality was added to main.js

--- static_folder/main.js
+++ static_folder/main.js
@@ -6,9 +6,19 @@ function handlerOut(){
  $(this).css("background-color","blue");
 }
 
+// Handlers used for transforming lyric elements into clickable button elements
+function handleInputLeft(){
+  console.log("Left side was clicked")
+}
+function handleInputRight(){
+  console.log("Right side was clicked")
+}
+
 
 function setupHandlers(){
   $('.section').hover(handlerIn,handlerOut);
+  $('#section-left').click(handleInputLeft)
+  $('#section-right').click(handleInputRight)
 }
 
 
