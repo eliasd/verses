3f63d211257253614cc337b6f6d8430d1e858642
Daniel Guillen <dguillen@Daniels-MacBook-Pro.local>
2017-08-27 19:52:30 -0700

    ajax functionality is now available on the right side of the page

--- static_folder/main.js
+++ static_folder/main.js
@@ -6,14 +6,14 @@ function handlerOut(){
  $(this).css("background-color","blue");
 };
 
-// Handlers used for using AJAX to send the data on the page to the server
+// Left side Handler uses AJAX to send the data on the page to the server
 function handlerVoteLeft(){
   $.ajax({
     type: "POST",
     url: "/vote/",
     dataType: 'json',
     // EVEN BIGGER NOTICE: YOU CAN SIMPLIFY THIS FURTHER BY: creating a function that alternates between left/right selection
-    // NOTICE: Modify 'data' so that it also sends the data from the unselected lyric
+    // broswer uses ajax to send the data from the unselected and selected lyric
     data: JSON.stringify(
       {
         "lyric-selected": $('#lyric-left').text(),
@@ -23,7 +23,7 @@ function handlerVoteLeft(){
         "song-name-unselected":$('#song-name-right').text(),
         "artist-name-unselected":$('#artist-name-right').text()
       })
-      // NOTICE: Modify data so that it updates BOTH the unselected and selected lyric with a new lyric
+      // This section updates BOTH the unselected and selected lyrics with new lyrics
   }).done(function( data ) {
     $('#lyric-span-left').text(data['lyric-selected']);
     $('#song-span-left').text(data['song-name-selected']);
@@ -35,9 +35,32 @@ function handlerVoteLeft(){
 
 };
 
-// NOTICE: AJAX Functionality has to be added to the Right-side section
+//AJAX Functionality for the right side handler
 function handlerVoteRight(){
-
+  $.ajax({
+    type: "POST",
+    url: "/vote/",
+    dataType: 'json',
+    // EVEN BIGGER NOTICE: YOU CAN SIMPLIFY THIS FURTHER BY: creating a function that alternates between left/right selection
+    // Data sends the data from the unselected and selected lyric
+    data: JSON.stringify(
+      {
+        "lyric-selected": $('#lyric-right').text(),
+        "song-name-selected": $('#song-name-right').text(),
+        "artist-name-selected":$("#artist-name-right").text(),
+        "lyric-unselected":$('#lyric-left').text(),
+        "song-name-unselected":$('#song-name-left').text(),
+        "artist-name-unselected":$('#artist-name-left').text()
+      })
+      // This section updates BOTH the unselected and selected lyrics with new lyrics
+  }).done(function( data ) {
+    $('#lyric-span-right').text(data['lyric-selected']);
+    $('#song-span-right').text(data['song-name-selected']);
+    $('#artist-name-right').text(data['artist-name-selected']);
+    $('#lyric-span-left').text(data['lyric-unselected']);
+    $('#song-span-left').text(data['song-name-unselected']);
+    $('#artist-name-left').text(data['artist-name-unselected']);
+  });
 };
 
 
