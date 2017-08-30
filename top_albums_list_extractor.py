de25bd0bbef740aa688ddf4830036a577f52a955
Daniel Guillen <dguillen@Daniels-MacBook-Pro.local>
2017-08-30 16:00:21 -0700

    added third album retrieval function to top_albums_list_extractor.py

--- top_albums_list_extractor.py
+++ top_albums_list_extractor.py
@@ -86,4 +86,43 @@ def get_top_albums_billboard_2():
     except Exception as e:
         return "Exception occurred \n" +str(e)
 
-print get_top_albums_billboard_2()
+def get_top_albums_itunes():
+    url = "http://www.itunescharts.net/us/charts/albums/hip-hop/"
+    try:
+        content = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(content, 'html.parser')
+        album_page = str(soup)
+
+        albums_dict = {}
+        #current_index refers to the starting index at which album_page is searched
+        current_index = 0
+        #length of the tag right before the artist name
+        beginning_artist_front_tag_length = len('<span class="entry">')
+        artist_album_seperator_length = len(' - ')
+
+        no_more_albums = False
+        while(no_more_albums==False):
+            current_index = album_page.find('<span class="entry">',current_index)
+            if(current_index<0):
+                no_more_albums = True
+                break;
+            #the index that is immediately after the artist name ()
+            front_artist_end_tag_index = album_page.find(' - ',current_index)
+            #the artist name is sliced out from between the front tag and the seperator ' - '
+            artist_name = album_page[(current_index+beginning_artist_front_tag_length):front_artist_end_tag_index]
+
+            #the index at which the album name starts
+            end_album_front_tag_index = album_page.find(' - ',current_index)+artist_album_seperator_length
+            # #this is the the index immediately after the album name (end tag)
+            beginning_album_end_tag_index = album_page.find('<',end_album_front_tag_index)
+            #
+            album_name = album_page[end_album_front_tag_index:beginning_album_end_tag_index]
+            #
+            albums_dict[album_name] = artist_name
+            #current_index is increased by one so the program can continue to search along the page, downwards
+            current_index = current_index+1
+        return albums_dict
+    except Exception as e:
+        return "Exception occurred \n" +str(e)
+
+print get_top_albums_itunes()
