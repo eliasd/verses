8fbfd4452d31139bd3396856ed672165c30a14c9
Daniel Guillen <dguillen@Daniels-MacBook-Pro.local>
2017-09-01 18:41:45 -0700

    tracklist now retrieves from a different source

--- album_tracklist_extractor_and_selector.py
+++ album_tracklist_extractor_and_selector.py
@@ -5,10 +5,98 @@ import sys
 sys.path.insert(0, 'lib')
 from bs4 import BeautifulSoup
 import random
+from HTMLParser import HTMLParser
 
 # Part of 'album_track_lyric_search_functions'
 #finds the tracks within an album and then randomly selects one track and returns the title of that track
 def get_random_track(artist,album):
+    # This removes any 'deluxe' or 'remastered' tags from the album name
+    album = album.replace(' (Deluxe)','').replace(' (Deluxe Version)','').replace(' (Remastered)','').replace(' - EP','')
+    album = album.lower()
+    album = album.capitalize()
+    #the album name is split into a list in order to remove unnecessary characters for the url
+    album_split_list = album.split(" ")
+    album = ""
+    for n in range(0,len(album_split_list)):
+        album_split_list[n] = re.sub('[^A-Za-z0-9]+',"-",album_split_list[n])
+        len_of_word = len(album_split_list[n])
+        if n==0 and album_split_list[n][0:1] == "-":
+            album += album_split_list[n][1:]
+        elif len_of_word==1 and album_split_list[n] == "-":
+            continue
+        elif album_split_list[n][len_of_word-1:len_of_word] == "-" and n!=len(album_split_list)-1:
+            album += album_split_list[n]
+        elif n==len(album_split_list)-1 and album_split_list[n][len_of_word-1:len_of_word] == "-":
+            album += album_split_list[n][0:len_of_word-1]
+        elif n==len(album_split_list)-1 and album_split_list[n][len_of_word-1:len_of_word] != "-":
+            album += album_split_list[n]
+        else:
+            album += album_split_list[n]+"-"
+
+    artist = artist.lower()
+    artist = artist.capitalize()
+    # remove all except alphanumeric characters from artist
+    artist = re.sub('[^A-Za-z0-9]+', "-", artist)
+    url = "http://genius.com/albums/"+artist+"/"+album
+
+    try:
+        headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36' }
+        req = urllib2.Request(url,None,headers)
+        content = urllib2.urlopen(req).read()
+        soup = BeautifulSoup(content, 'html.parser')
+        album_page = str(soup)
+
+        # lyrics lies between up_partition and down_partition
+        up_partition = '<div class="column_layout u-top_margin">'
+        down_partition = '<div class="footer">'
+        album_page = album_page.split(up_partition)[1]
+        album_page = album_page.split(down_partition)[0]
+
+        #current_index is first set to the index at which the name of the album is found
+        current_index = album_page.find('<h3 class="chart_row-content-title">')
+        len_front_tag = len('<h3 class="chart_row-content-title">')
+        track_list = []
+        no_more_tracks = False
+        while(no_more_tracks==False):
+            #current_index is set to the first front-tag index as it is necessary in
+            # # order to find the end-index of the front tag (right before the artist name)
+            current_index = album_page.find('<h3 class="chart_row-content-title">',current_index)
+            if current_index < 0:
+                break
+            #this is the the index immediately after the track name (end tag)
+            album_end_index = album_page.find('<span',current_index)
+
+            #extracts the song title and removes annoying whitespace
+            song_title = album_page[current_index+len_front_tag:album_end_index].strip()
+            #if the song has any features it removes the string of the 'feat'
+            song_title = song_title.replace("(Album Version)","")
+            if song_title.find(' (Ft.')>=0:
+                front_index = song_title.find('(Ft.')
+                song_title = song_title[0:front_index]
+
+            song_title = song_title.strip()
+            # #Decoding html entities in python string by simply replacing it
+            #NOTICE: CONSIDER moving the decoding into main.py
+            # song_title = song_title.replace('&amp;','&')
+            song_title = song_title.replace('\xc2','').replace('\xa0','').replace('\xe2','').replace("\x80",'').replace('\x8b','')
+            track_list.append(song_title)
+            # current_index is updated to proceed down the html page
+            current_index=current_index+1
+            # checks to make sure that its the last track on the album
+            check_for_track_index = album_page.find('<span class="chart_row-number_container-number chart_row-number_container-number',current_index)
+            end_check_index = album_page.find('</span>',check_for_track_index)
+            if album_page.find('<span></span>',check_for_track_index,end_check_index+10)>=0:
+                break
+
+
+        #at this point, track_list has been completely filled with the all tracks related to album at hand
+        track_length = len(track_list)
+        random_index = random.randint(0,track_length-1)
+        return track_list[random_index]
+    except Exception as e:
+        return "Exception occurred \n"
+
+def get_random_track2(artist,album):
     # This removes any 'deluxe' or 'remastered' tags from the album name
     album = album.replace(' (Deluxe)','').replace(' (Deluxe Version)','').replace(' (Remastered)','')
     artist = artist.lower()
@@ -65,4 +153,4 @@ def get_random_track(artist,album):
     except Exception as e:
         return "Exception occurred \n"
 
-# print get_random_track('Bryson Tiller','T R A P S O U L')
+print get_random_track('Kendrick Lamar','DAMN.')
